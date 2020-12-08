442711655dfc65c7c74e4744196b0d4248881d92
Ifra Idrees <ifraidrees.ngxoft@gmail.com>
2020-12-09 01:18:12 +0500

    Updated

--- app/src/main/java/com/example/contactlist/MainActivity.java
+++ app/src/main/java/com/example/contactlist/MainActivity.java
@@ -1,10 +1,5 @@
 package com.example.contactlist;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -12,6 +7,11 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.contactlist.Adapters.ContactAdapter;
 import com.example.contactlist.ModelClass.ContactModelClass;
@@ -32,13 +32,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        contact = findViewById(R.id.contact);
+        recyclerView = findViewById(R.id.recycler_view);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        ContactAdapter contactAdapter = new ContactAdapter(this, getContact());
+        recyclerView.setAdapter(contactAdapter);
 
-        initField();
 
-
-
-
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_CONTACTS) !=
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_CONTACTS) !=
                 PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, 1);
         } else {
@@ -47,15 +49,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void initField() {
-
-        contact = findViewById(R.id.contact);
-        recyclerView = findViewById(R.id.recycler_view);
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-        ContactAdapter contactAdapter = new ContactAdapter(this, getContact());
-        recyclerView.setAdapter(contactAdapter);
-    }
 
     private List<ContactModelClass> getContact() {
 
@@ -65,16 +58,16 @@ public class MainActivity extends AppCompatActivity {
             String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
             String mobile = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
 
-            contactList.add(new ContactModelClass(name,mobile));
+            contactList.add(new ContactModelClass(name, mobile));
         }
         return contactList;
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode==1){
+        if (requestCode == 1) {
 
-            if (grantResults[0]==PackageManager.PERMISSION_GRANTED){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 getContact();
             }
 
